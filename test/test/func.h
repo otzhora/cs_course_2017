9ac04dd00883b5ed495b9fbfd6200ed1481e5261
Yuriy Rogachev <y_rogachev@inbox.ru>
2017-12-17 19:57:57 +0300

    xcode

---
+++ test/test/func.h
@@ -0,0 +1,8 @@
+#ifndef func_h
+#define func_h
+
+int sqr(int& n){
+    return n*n;
+}
+
+#endif /* func_h */
---
+++ test/test/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "func.h"
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int n;
+    cin >> n;
+    cout << sqr(n) << endl;
+    return 0;
+}
