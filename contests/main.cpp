61ec98e49297dd18605fff5afec6d6f1100c7b5a
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-02-17 17:42:26 +0300

    contests placeholder

---
+++ contests/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+using namespace std;
+
+typedef unsigned long long ull;
+
+int main() {
+    freopen("input.txt", "rt", stdin);
+    freopen("output.txt", "wt", stdout);
+    
+    
+    return 0;
+}
