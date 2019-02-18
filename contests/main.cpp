42cecec9a02d76efcb19f0b1ea1216fdc33ccd5c
Yuriy Rogachev <y_rogachev@inbox.ru>
2019-02-18 17:04:16 +0300

    new contest template

--- contests/main.cpp
+++ contests/main.cpp
@@ -1,15 +1,24 @@
+#define ALL(a) a.begin(), a.end()
+#define RALL(a) a.rbegin(), a.rend()
+#define mp(a, b) make_pair(a, b)
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <map>
+#include <set>
+#include <string> 
 using namespace std;
 
 typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int, int> pii;
+
 
 int main() {
     freopen("input.txt", "rt", stdin);
     freopen("output.txt", "wt", stdout);
     
-    
+
     return 0;
 }
